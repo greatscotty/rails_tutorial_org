68fec3449e1d91bb1f5f514cf14e4ceab64781c3
Scott Jensen <scott.jensen@shopify.com>
2018-04-01 22:53:10 -0400

    Create hello_world application

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "<p>Hello, world!<br> ¡Hola, mundo!</p>".html_safe
+  end
+
+  def goodbye
+    render html: "Goodbye, World!"
+  end
+
 end
